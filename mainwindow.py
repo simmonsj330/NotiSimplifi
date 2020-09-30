7f87958fffec8ef3d1d6b4e39812970527ce63ff
Terryl Dodson <tdodson3@vols.utk.edu>
2020-09-30 18:00:51 -0400

    modified:   mainwindow.py

--- mainwindow.py
+++ mainwindow.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 688)
+        MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
