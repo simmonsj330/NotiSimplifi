4d39ee19ade06f7adb7c8af0324b9e67fbb6952c
Ryan Stewart <rstewa35@vols.utk.edu>
2020-09-30 21:28:14 -0400

    closed #12

--- newmain.py
+++ newmain.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPixmap, QPainter
+from PyQt5.QtWidgets import QSplashScreen
+from PyQt5.QtCore import QEventLoop, QTimer, Qt
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -99,8 +103,28 @@ class Ui_MainWindow(object):
 if __name__ == "__main__":
 
     app = QtWidgets.QApplication(sys.argv)
+    
+    # Splash screen & logo
+    logo_path = os.path.dirname(os.path.realpath(__file__))
+    pixmap = QPixmap('/'.join([logo_path, 'resources/logo.png']))
+    splashScreen = QSplashScreen(pixmap, Qt.WindowStaysOnTopHint)
+    splashScreen.show()
+
+    # this checks for button clicks while main form is loading
+    # keeping for future use
+    app.processEvents()
+
+		# delay main form from showing until 2 seconds has passed
+    loop = QEventLoop()
+    QTimer.singleShot(2000, loop.quit)
+    loop.exec_()
+    
     w = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(w)
+    
     w.show()
+    
+		# stop showing splashscreen once main form has loaded in
+    splashScreen.finish(w)
     sys.exit(app.exec_())# -*- coding: utf-8 -*-
