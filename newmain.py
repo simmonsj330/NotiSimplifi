2191935fbc172422e8bd4b6f80193451505597e7
Ryan Stewart <rstewa35@vols.utk.edu>
2020-09-30 21:52:04 -0400

    fixed image path for menu bar after changing it in commit #12

--- newmain.py
+++ newmain.py
@@ -22,7 +22,7 @@ class Ui_MainWindow(object):
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(10, 10, 51, 51))
         self.label.setText("")
-        self.label.setPixmap(QtGui.QPixmap("logo.png"))
+        self.label.setPixmap(QtGui.QPixmap("resources/logo.png"))
         self.label.setScaledContents(True)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
